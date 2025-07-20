acaba9510bbacb3d74d19561d4fd23f6cf43badf
ThanhLT2611 <thanh25msa13225@fsb.edu.vn>
2025-07-20 20:47:24 +0700

    feat: add upload_file api but FE cant show

--- app/api/exam_api.py
+++ app/api/exam_api.py
@@ -1,11 +1,15 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, FastAPI
 from fastapi.responses import JSONResponse
 from typing import List
+import os
+
+from fastapi.staticfiles import StaticFiles
 
 from app.schema.schema import ExamResponse, IResponseBase, SubmitExamRequest, SubmitExamResponse
 from app.service.exam_service import get_exam_by_subject_code, get_exam_by_exam_code, update_exam_by_exam_code, submit
+from uuid import uuid4
 
 exam_route = APIRouter()
 
@@ -56,3 +60,18 @@ def submit_exam(request: SubmitExamRequest):
             items=response
         ).model_dump()
     )
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+exam_route.mount("/uploads", StaticFiles(directory="uploads"), name="images")
+
+@exam_route.post("/files/upload",response_model=str)
+async def upload_question_file(file: UploadFile = File(...)):
+    ext = os.path.splitext(file.filename)[1]
+    file_id = f"{uuid4().hex}{ext}"
+    file_path = os.path.join(UPLOAD_FOLDER, file_id)
+    with open(file_path, "wb") as f:
+        print(1)
+        f.write(await file.read())
+    return JSONResponse(content={"file_id": file_id})
+
